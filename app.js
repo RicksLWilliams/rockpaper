42e41bcedcfa77347066a90641ab3c787419d0d6
RicksLWilliams <rlwilliamsix@yahoo.com>
2020-03-19 14:17:43 -0500

    added code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('app.js load')
 
 function play(playerChoice) {
   var computerChoise = chooseItemList()
-  var result = "start here"
+  var result = "Logic needed for this condition"
 
   if (playerChoice == computerChoise) {
     result = "tied"
@@ -20,7 +20,8 @@ function play(playerChoice) {
     console.log("How did you get here?")
   }
 
-  alert( "player: " + playerChoice + " computer: " + computerChoise + " You " + result)
+  //alert( "player: " + playerChoice + " computer: " + computerChoise + " You " + result)
+  draw("Player( " + playerChoice + " )  Computer( " + computerChoise + " )  You " + result)
   return result
 }
 
@@ -41,8 +42,67 @@ function chooseItemIf() {
 }
 
 
-let items = ["rock", "paper", "scissors"]
+let items = ["rock", "paper", "scissors","next1"]
 
 function chooseItemList() {
   return items[Math.floor(Math.random() * items.length)]  
 }
+
+function draw(result) {
+  let resultElem = document.getElementById("result")
+
+  resultElem.innerText = result
+} 
+
+
+function drawButtons(){
+  let template = ``
+ // <button class="btn btn-warning btn-lg p-5 m-2" onclick="play('scissors')">Scissors</button>`
+
+//players.sort((p1, p2) => p2.topScore- p1.topScore)
+
+  items.forEach(item => {
+    template += `
+    <button class="btn btn-warning btn-lg p-5 m-2" onclick="play('${item}')"> ${item}</button>`
+  })
+
+  document.getElementById("buttons").innerHTML = template
+
+}
+
+//
+
+function nextDirecion(curDir, changeDir){
+
+  if (changeDir == "R") {
+    curDir ++
+    if (curDir == 4) {
+      curDir = 0
+    }
+  } else if (changeDir = "L") {
+    curDir--
+    if (curDir == -1) {
+      curDir = 3
+    } 
+  }
+
+}
+
+
+function go (arr){
+  let directions = ["N", "E", "S", "w"]
+  let curDir = 0
+
+  for (let index = 0; index < arr.length; index++) {
+    curDir = nextDirecion(curDir, )
+  }
+
+}
+
+
+
+
+
+
+
+
